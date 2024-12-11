2080909946e53922f3708648487b8aa98560eb2d
Yuvaraj Dekhane <148527838+yuvaraj8080@users.noreply.github.com>
2024-12-12 02:00:59 +0530

    this new chatgpt chat bot app

--- lib/data/remote/api/urls.dart
+++ lib/data/remote/api/urls.dart
@@ -1,5 +1,5 @@
 class Urls{
   static const String BASE_URL = "https://api.openai.com/v1/";
   static const String CHAT_COMPLETION_API = "${BASE_URL}chat/completions";
-  static const String CHAT_COMPLETION_API_KEY = "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";
+  static const String CHAT_COMPLETION_API_KEY = "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";
 }
